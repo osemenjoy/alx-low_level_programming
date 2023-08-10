413dc4f36b0bc7cffc020cdc86dc6e7172f51a5c
osemenjoy <osemenjoy45@gmail.com>
2023-08-10 10:05:52 -0700

    C programming begins

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line to stanadard error
+ * Return: 1
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
