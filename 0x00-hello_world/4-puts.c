1f57a6796f70cc7f3376e5923895f72f80164fae
osemenjoy <osemenjoy45@gmail.com>
2023-08-10 05:50:30 -0700

    C programming begins

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that prints out a text using the function puts
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
