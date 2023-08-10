bc9b4d97eb4f976d277812d6b93e332df0fb5d62
osemenjoy <osemenjoy45@gmail.com>
2023-08-10 09:47:44 -0700

    C programming begins

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,10 @@ int main(void)
 	long int c;
 	long long int d;
 	float f;
-printf("size of char: %lu byte(s)\n", (unsigned long)sizeof(a));
-printf("size of int: %lu byte(s)\n", (unsigned long)sizeof(b));
-printf("size of long int: %lu bytes(s)\n", (unsigned long)sizeof(c));
-printf("size of long long int: %lu bytes(s)\n", (unsigned long)sizeof(d));
-printf("size of float: %lu byte(s)\n", (unsigned long)sizeof(f));
+printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("size of a long int: %lu bytes(s)\n", (unsigned long)sizeof(c));
+printf("size of a long long int: %lu bytes(s)\n", (unsigned long)sizeof(d));
+printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
