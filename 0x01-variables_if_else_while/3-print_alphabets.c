f4631a253bfa57da9cad4ee2798482560342fdd7
osemenjoy <osemenjoy45@gmail.com>
2023-08-11 07:15:09 -0700

    if-else-while statements in C

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char aph[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	;int i;
+
+	for (i = 0; i < 52; i++)
+	{
+		putchar(aph[i]);
+	}
+	putchar('\n');
+	return (0);
+}
