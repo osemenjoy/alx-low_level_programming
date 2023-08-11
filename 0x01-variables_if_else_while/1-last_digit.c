168598c36eec33362c5ee0baad87e4a54ecfa93c
osemenjoy <osemenjoy45@gmail.com>
2023-08-11 06:48:19 -0700

    if-else-while statements in C

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,7 +2,8 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main - Prints the last digit of a randomly generated number and whether it is greater than 5, less than 6 or 0.
+ * main - Prints the last digit of a randomly generated number,
+ * whether it is greater than 5, less than 6 or 0.
  * Return: 0 (success)
  */
 int main(void)
@@ -11,7 +12,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-        if ((n % 10) > 5)
+	if ((n % 10) > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
 	}
@@ -19,7 +20,7 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 	}
-	else 
+	else
 	{
 		printf("Last digit of %d is %d and is 0\n", n, n % 10);
 	}
