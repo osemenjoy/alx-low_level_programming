5a694ac67624233c9ad1bf68dacdacc34386f144
osemenjoy <osemenjoy45@gmail.com>
2023-08-15 17:01:41 -0700

    functions and nested loops in C

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,10 +8,11 @@ int main(void)
 	int sum = 0;
 	int i;
 
-	for (i = 1; i < 10; i++)
+	for (i = 1; i < 1024; i++)
 	{
 		if (i % 3 == 0 || i % 5 == 0)
 			sum = sum + i;
 	}
 	printf("%d\n", sum);
+	return (0);
 }
