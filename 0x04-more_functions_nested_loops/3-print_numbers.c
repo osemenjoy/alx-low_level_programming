d66cf2414e55578bc280a7d39d5b7740f59299c1
osemenjoy <osemenjoy45@gmail.com>
2023-08-17 16:03:05 -0700

    more on functions and nested loops in C

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,8 +1,16 @@
 #include "main.h"
 /**
- * _isupper - checks for lowercase character
- * @c - character to be checked
- * Return: 1 for uppercase character and 0 for anything else
+ * print_numbers - prints numbers from 0 to 9
+ * Return:0
  */
 
 void print_numbers(void)
+{
+char c;
+
+for (c = '0'; c <= 9; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
