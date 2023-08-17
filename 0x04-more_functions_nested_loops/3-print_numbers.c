47d7ed738c11e3fc2900827dce9ac585674d3af1
osemenjoy <osemenjoy45@gmail.com>
2023-08-17 15:56:38 -0700

    more on functions and nested loops in C

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,8 @@
+#include "main.h"
+/**
+ * _isupper - checks for lowercase character
+ * @c - character to be checked
+ * Return: 1 for uppercase character and 0 for anything else
+ */
+
+void print_numbers(void)
