f317e871e658d2363a67f4a7ad53c9dc5d0434ea
osemenjoy <osemenjoy45@gmail.com>
2023-08-23 09:40:22 -0700

    pointers, strings and array in C

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -9,7 +9,7 @@ int *p;
 a[2] = 1024;
 p = &n;
   /*
-   * main: adds line to the code
+   * write your line of code here...
    * Remember:
    * - you are not allowed to use a
    * - you are not allowed to modify p
